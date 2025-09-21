ddadfc33c83c70c4f04a87ce160304983abcfb15
xXemran05khanXx <xemran05khanx@gmail.com>
2025-09-21 23:33:10 +0530

    Update MongoDB connection with working Cluster0 credentials

--- backend/keys.js
+++ backend/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoUrl: process.env.MONGO_URL || "mongodb+srv://yashprafulborse:tPJkv8VzD2mZTAQ0@xenith.miujsji.mongodb.net/xenith?retryWrites=true&w=majority",
+    mongoUrl: process.env.MONGO_URL || "mongodb+srv://xemran05khanx_db_user:22104049@cluster0.2jrtg.mongodb.net/xenith?retryWrites=true&w=majority&appName=Cluster0",
     Jwt_secret: process.env.JWT_SECRET || "faslkfocvneofu"
 };
